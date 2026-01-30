2a51e2e5cdb2639d5e445323ea0d75df35dd32fc
dadyarri <dadyarri@gmail.com>
2026-01-30 21:25:52 +0300

    add time column to response

--- PlannerBot/Data/Response.cs
+++ PlannerBot/Data/Response.cs
@@ -1,11 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PlannerBot.Data;
 
 public class Response
 {
     public long Id { get; set; }
     public DateOnly Date { get; set; }
+    public TimeOnly? Time { get; set; }
     public required User User { get; set; }
     public required Availability? Availability { get; set; }
 }

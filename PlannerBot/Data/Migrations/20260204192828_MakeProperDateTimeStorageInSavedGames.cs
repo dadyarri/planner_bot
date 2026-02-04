e15c8f304071d33245611ec1e0e57748464008cd
dadyarri <dadyarri@gmail.com>
2026-02-04 22:31:01 +0300

    migrate separate datetime columns to single in saved games too

---
+++ PlannerBot/Data/Migrations/20260204192828_MakeProperDateTimeStorageInSavedGames.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlannerBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeProperDateTimeStorageInSavedGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTime",
+                table: "SavedGame",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+            
+            migrationBuilder.Sql("""
+                                 UPDATE "SavedGame"
+                                 SET "DateTime" = ("Date" + "Time") - INTERVAL '3 hour'
+                                 """);
+            
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "SavedGame");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "SavedGame");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTime",
+                table: "SavedGame");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "SavedGame",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "Time",
+                table: "SavedGame",
+                type: "time without time zone",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+    }
+}
--- PlannerBot/Data/SavedGame.cs
+++ PlannerBot/Data/SavedGame.cs
@@ -3,6 +3,5 @@ namespace PlannerBot.Data;
 public class SavedGame
 {
     public int Id { get; set; }
-    public DateOnly Date { get; set; }
-    public TimeOnly Time { get; set; }
+    public DateTime DateTime { get; set; }
 }
